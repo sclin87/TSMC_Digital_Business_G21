ac7f3581cc036923850e4fb46f48f899575adab0
sclin87 <sclin87.cs07@nycu.edu.tw>
2022-06-04 15:16:06 +0800

    Change search range to day by day from 2019

--- URL_Manager/URL_Gen.py
+++ URL_Manager/URL_Gen.py
@@ -3,11 +3,11 @@ from requests_html import HTMLSession
 import requests, schedule
 import sys, os, socket, time, datetime
 
-#service_host = os.getenv("SERVICE_HOST")
-#service_port = int(os.getenv("SERVICE_PORT"))
+service_host = os.getenv("SERVICE_HOST")
+service_port = int(os.getenv("SERVICE_PORT"))
 #service_host = "140.113.68.204"
-service_host = "localhost"
-service_port = 7878
+#service_host = "localhost"
+#service_port = 7878
 
 class UrlGenerator():
     def __init__(self):
@@ -15,7 +15,7 @@ class UrlGenerator():
         self.queries = ["TSMC", "Applied+Materials", "ASML", "SUMCO"]
 
     # Google search with queries and parameters
-    def google_search(self, query, time='qdr:m', num=100, retry=10):
+    def google_search(self, query, time='qdr:d', num=100, retry=5):
         search_url = self.url + query + \
             '&tbm=nws&tbs=%s&num=%d&lr=lang_en' % (time, num)
         response = self.get_source(search_url)
@@ -53,59 +53,60 @@ class UrlGenerator():
         return results
 
     def generate_url(self, date, num=10):
-        print(date.strftime('%Y-%m-%d'))
+        search_time = self.get_google_search_date(date)
         links = []
         for query in self.queries:
-            results = self.google_search(query, time='qdr:m', num=num)
+            results = self.google_search(query, time=search_time, num=num)
             for res in results:
                 if res['link'] not in results:
                     links.append(res['link'])
         return links
 
-    def get_google_search_date(date):
-        year = date.year
-        month = date.month
-        day = date.day
-        print('%d-%d-%d' % (year, month, day))
+    def get_google_search_date(self, date):
+        if date == datetime.date.today():
+            return 'qdr:d'
+        else:
+            return 'cdr%3A1%2Ccd_min%3A{month}%2F{day}%2F{year}%2Ccd_max%3A{month}%2F{day}%2F{year}'.format(
+                month=date.month, day=date.day, year=date.year
+            )
 
 # Get Current Time (UTC+8)
 def cur_time_str():
     tz = datetime.timezone(datetime.timedelta(hours=8))
     return datetime.datetime.now(tz=tz).strftime('%Y-%m-%d %H:%M:%S')
 
-def send_links(links):
+def send_links(links, date):
     try:
         print('Sending Links to %s:%d' % (service_host, service_port))
         for link in links:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect((service_host, service_port))
             print('-', link)
-            outstr = link + '\n'
+            outstr = date.strftime('%Y-%m-%d') + ' ' + link + '\n'
             sock.sendall(outstr.encode('ascii'))
             sock.close()
     except socket.error as e:
         print(e, file=sys.stderr)
         os._exit(1)
 
-base_date = datetime.date(2019, 1, 1) # (YYYY, MM, DD)
+base_date = datetime.date(2019, 1, 1) # (Y, M, D)
 target_date = base_date
 
 # Repeat the Job
 @schedule.repeat(schedule.every(30).seconds)
 def job():
     print(cur_time_str())
-
     global target_date
     generator = UrlGenerator()
-    results = generator.generate_url(target_date, num=10)
-    send_links(results)
+    results = generator.generate_url(target_date, num=20)
+    send_links(results, target_date)
     target_date = target_date + datetime.timedelta(days=1)
 
 if __name__ == '__main__':
     # Initial Job
     job()
 
-    # Check pending job every 5 minutes
+    # Check pending job before sleep
     while True:
         schedule.run_pending()
         time.sleep(10)
